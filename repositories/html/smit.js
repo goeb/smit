ae17a845cfe2d88c25219a60114202fb5b73ac7e
fred <fred@fred1>
2013-09-26 18:36:39 +0200

    added smit.js (forgotten)

---
+++ repositories/html/smit.js
@@ -0,0 +1,28 @@
+
+// The HTML page must include the smit_data
+// <script id="smit_data" type="application/json">
+//      {"smit_user": "Mr. John", "smit_issueNumber": 34}
+// </script>
+// 
+// and where to display this information:
+// Logged in as <span class="smit_user">nobody</span>
+
+function update(classname, value)
+{
+    var els = document.getElementsByClassName(classname);
+    for(var i=0; i<els.length; i++) els[i].innerHTML = value;
+}
+
+function updateFields()
+{
+    var data = JSON.parse(document.getElementById("smit_data").innerHTML);
+    for(var key in data) update(key, data[key]);
+}
+
+function changeWrapping(){
+    var msg = document.getElementsByName('message')[0];
+    if (msg.wrap != "off") msg.wrap = "off";
+    else msg.wrap = "hard";
+}
+
+window.onload = updateFields;
