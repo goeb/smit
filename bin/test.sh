ca4c6b6293e53c4a9417c27d721dc45c6d9ef962
fred <fred@fred1>
2013-11-06 22:08:36 +0100

    add tickets for the demo

--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,4 @@
 
 ./smitc  signin http://127.0.0.1:8080 homer homer
 
-for i in `seq 1 ^C00`; do ./smitc post http://127.0.0.1:8080 things_to_do new "summary=test$i\&in_charge=toto$i\&status=open"; done
+for i in `seq 1 1000`; do ./smitc post http://127.0.0.1:8080 things_to_do new "summary=test$i\&in_charge=toto$i\&status=open\&%2Bmessage=`date | sed -e "s/ /_/g"`"; done
