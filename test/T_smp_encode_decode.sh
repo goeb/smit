b7632b249b157e0b7c039e0293fa4ea1873c5118
fred <fhoerni@free.fr>
2014-06-15 21:47:15 +0200

    PASS: T_smp_encode_decode.sh

--- test/T_smp_encode_decode.sh
+++ test/T_smp_encode_decode.sh
@@ -4,13 +4,18 @@
 
 set -e
 TMP=T_smp_encode_decode.out
-for i in `seq 1 10000`; do 
-    echo i=$i
+N=10000
+echo "Running $N random combinations..."
+for i in `seq 1 $N`; do 
+    modulo=`expr $i % 1000 || echo >/dev/null` # prevent exit code 1 from expr
+    if [ $modulo -eq 0 ]; then
+        echo $i...
+    fi
     echo -n "smp.1.1 " > $TMP.ref
-    ./get_random_value $i >> $TMP.ref
+    ../get_random_value $i >> $TMP.ref
     echo >> $TMP.ref
 
-    ./get_random_value $i | ../bin/smparser -e | ../bin/smparser - > $TMP
+    ../get_random_value $i | ../smparser -e | ../smparser - > $TMP
 
     diff $TMP.ref $TMP
 done
--- test/get_random_value.c
+++ test/get_random_value.c
@@ -1,4 +1,5 @@
-
+#include <ctype.h>
+#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 
