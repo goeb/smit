cffbfafe317edd5983d996178e59a4d13cd16d63
fred <fhoerni@free.fr>
2014-06-30 22:27:01 +0200

    clone in progress.

--- src/clone.cpp
+++ src/clone.cpp
@@ -12,7 +12,11 @@
  */
 #include "config.h"
 
+#include <string>
+#include <list>
 #include <stdio.h>
+#include <curl/curl.h>
+#include <stdlib.h>
 
 #include "clone.h"
 
@@ -31,13 +35,164 @@ int helpClone()
            "Example:\n"
            "  smit clone http://example.com:8090 localDir\n"
            "\n"
-          );
+           );
+    return 0;
+}
+
+
+
+size_t writeMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
+size_t receiveLinesCallback(void *contents, size_t size, size_t nmemb, void *userp);
+
+class HttpRequest {
+public:
+    HttpRequest();
+    ~HttpRequest();
+    void handleReceivedLines(const char *contents, size_t size);
+    void handleReceivedRaw(void *data, size_t size);
+    void setUrl(const char *u);
+    void getRequestLines();
+    void getRequestRaw();
+
+private:
+    void performRequest();
+    std::string url;
+    std::string response;
+    std::list<std::string> lines;
+    std::string currentLine;
+    CURL *curlHandle;
+};
+
+int getProjects(const char *rooturl)
+{
+    HttpRequest hr;
+
+    hr.setUrl(rooturl);
+    hr.getRequestLines();
+
     return 0;
 }
 
 
 int cmdClone(int argc, const char **args)
 {
+    if (argc < 2) {
+        printf("You must specify a repository to clone and a directory.\n\n");
+        helpClone();
+        return 1;
+    }
+    if (argc > 2) {
+        printf("Too many arguments.\n\n");
+        helpClone();
+        return 1;
+    }
+
+    const char *url = args[0];
+    const char *dir = args[1];
+
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    // TODO
+
+    getProjects(url);
+
+
+    curl_global_cleanup();
+
     return 1;
 }
+
+
+void HttpRequest::setUrl(const char *u)
+{
+    printf("setUrl(%s)", u);
+    url = u;
+    curl_easy_setopt(curlHandle, CURLOPT_URL, u);
+}
+
+
+void HttpRequest::getRequestRaw()
+{
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, writeMemoryCallback);
+    performRequest();
+}
+
+
+void HttpRequest::getRequestLines()
+{
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, receiveLinesCallback);
+    performRequest();
+}
+void HttpRequest::performRequest()
+{
+    CURLcode res;
+    res = curl_easy_perform(curlHandle);
+
+    if(res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s (url %s)\n", curl_easy_strerror(res), url.c_str());
+        exit(1);
+    }
+}
+
+size_t writeMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    HttpRequest *hr = (HttpRequest*)userp;
+    size_t realsize = size * nmemb;
+    hr->handleReceivedRaw(contents, realsize);
+    return realsize;
+}
+
+void HttpRequest::handleReceivedRaw(void *data, size_t size)
+{
+    response.append((char*)data, size);
+}
+
+
+size_t receiveLinesCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    HttpRequest *hr = (HttpRequest*)userp;
+    size_t realsize = size * nmemb;
+    hr->handleReceivedLines((char*)contents, realsize);
+    return realsize;
+}
+
+void HttpRequest::handleReceivedLines(const char *data, size_t size)
+{
+    if (data == 0) {
+        // finalize the currentLine and return
+        lines.push_back(currentLine);
+        currentLine.clear();
+    }
+    size_t i = 0;
+    size_t notConsumedOffset = 0;
+    while (i < size) {
+        if (data[i] == '\n') {
+            // clean up possible \r before the \n
+            size_t endl = i;
+            if (i > 0 && data[i-1] == '\r') endl--;
+
+            currentLine.append(data + notConsumedOffset, endl-notConsumedOffset);
+            lines.push_back(currentLine);
+            currentLine.clear();
+            notConsumedOffset = i+1;
+        }
+        i++;
+    }
+    if (notConsumedOffset < size) currentLine.append(data, notConsumedOffset, size-notConsumedOffset);
+}
+
+
+HttpRequest::HttpRequest()
+{
+    curlHandle = curl_easy_init();
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, (void *)this);
+    curl_easy_setopt(curlHandle, CURLOPT_USERAGENT, "smit-agent/1.0");
+}
+
+HttpRequest::~HttpRequest()
+{
+    curl_easy_cleanup(curlHandle);
+
+}
+
 
