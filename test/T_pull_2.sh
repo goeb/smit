a4cdfc08351dfd4add23f097225f648bcf93dc77
Frederic Hoerni <fhoerni@free.fr>
2015-01-10 11:40:54 +0100

    test rewritten using smit-issue

--- test/T_pull_2.sh
+++ test/T_pull_2.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # test smit pull with merge conflicts
+TEST_NAME=`basename $0`
+# remove suffix .sh
+TEST_NAME=`echo $TEST_NAME | sed -e "s/\.sh//"`
 
 SMIT=../smit
 
@@ -17,14 +20,20 @@ init() {
     $SMIT init $REPO
     $SMIT project -c $PROJECT1 -d $REPO
     $SMIT user $USER1 --passwd $PASSWD1 --project $PROJECT1:rw -d $REPO
+    # add custom property
+    chmod u+w $REPO/$PROJECT1/project
+    echo "addProperty freeText text" >> $REPO/$PROJECT1/project
 
     # create some entries
-    mkdir $REPO/$PROJECT1/issues/1
-    echo +parent null > $REPO/$PROJECT1/issues/1/entry1x1
-    echo +parent entry1x1 > $REPO/$PROJECT1/issues/1/entry1x2
-    mkdir $REPO/$PROJECT1/issues/2
-    echo +parent null > $REPO/$PROJECT1/issues/2/entry2x1
-    echo file1 > $REPO/$PROJECT1/files/file1
+    # create issue 1
+    $SMIT issue $REPO/$PROJECT1 -a - "summary=first issue" freeText="creation of issue1"
+    $SMIT issue $REPO/$PROJECT1 -a 1 status=open +message="some text...."
+
+    # create issue 2
+    $SMIT issue $REPO/$PROJECT1 -a - "summary=second issue" freeText="creation of issue2"
+    $SMIT issue $REPO/$PROJECT1 -a 2 status=open +message="some text (issue2)...."
+    $SMIT issue $REPO/$PROJECT1 -a 2 status=open +message="add a file" +file="a_file.txt"
+    echo file1_data_yy > $REPO/$PROJECT1/files/a_file.txt
 }
 cleanup() {
     REPO=trepo # just to be sure before the rm -rf
@@ -47,22 +56,6 @@ fail() {
     [ -n "$pid" ] && kill $pid
     exit 1
 }
-checkFile() {
-    not=0
-    if [ "$1" = "-not" ]; then
-        not=1
-        shift
-    fi
-    file="$1"
-    if [ "$not" = "1" ]; then
-        [ -f $file ] && fail "missing file '$file'"
-        echo "$file: ok (does not exist)"
-    else
-        [ -f $file ] || fail "missing file '$file'"
-        echo "$file: ok"
-    fi
-
-}
 
 cleanup
 init
@@ -78,28 +71,20 @@ cd -
 stopServer
 
 # server side: add entries
-ENTRY=$REPO/$PROJECT1/issues/1/entry1x3server
-echo +parent entry1x2 > $ENTRY
-echo "dummyProp dummyValueServer" >> $ENTRY
+$SMIT issue $REPO/$PROJECT1 -a 1 freeText=dummyValueServer
 # add an issue
-mkdir $REPO/$PROJECT1/issues/3
-echo +parent null > $REPO/$PROJECT1/issues/3/entry3x1server
-echo +parent entry3x1server > $REPO/$PROJECT1/issues/3/entry3x2server
+$SMIT issue $REPO/$PROJECT1 -a - freeText=x1server summary="third issue"
+$SMIT issue $REPO/$PROJECT1 -a 3 freeText=x2server
 
 # local side: add conflicting entries
-ENTRY=clone1/$PROJECT1/issues/1/entry1x3local
-echo +parent entry1x2 > $ENTRY
-echo "dummyProp dummyValueLocal" >> $ENTRY
-echo "otherProp otherValue" >> $ENTRY
+$SMIT issue clone1/$PROJECT1 -a 1 freeText=dummyValueLocal +message="local conflicting entry"
 # another entry
-ENTRY=clone1/$PROJECT1/issues/1/entry1x4local
-echo +parent entry1x3local  > $ENTRY
-echo "dummyProp newValue" >> $ENTRY
+$SMIT issue clone1/$PROJECT1 -a 1 freeText=newValueLocal
 
 # local side: add conflicting issue
-mkdir clone1/$PROJECT1/issues/3
-echo +parent null > clone1/$PROJECT1/issues/3/entry3x1local
-echo +parent entry3x1local > clone1/$PROJECT1/issues/3/entry3x2local
+$SMIT issue clone1/$PROJECT1 -a - freeText=x1local summary="local conflicting issue"
+$SMIT issue clone1/$PROJECT1 -a 3 freeText=x2local
+$SMIT issue clone1/$PROJECT1 -a 3 freeText=x3local
 
 startServer
 cd clone1
@@ -108,28 +93,13 @@ cd -
 stopServer
 
 # check that the new entry and the new issue are pulled correctly
-ISSUE=clone1/$PROJECT1/issues/1
-checkFile $ISSUE/entry1x1
-checkFile $ISSUE/entry1x2
-checkFile $ISSUE/entry1x3server
-checkFile -not $ISSUE/entry1x3local
-checkFile -not $ISSUE/entry1x4local
-n=`ls $ISSUE | wc -l`
-[ $n -eq 5 ] || fail "[$ISSUE] n=$n"
+(
+    $SMIT issue clone1/$PROJECT1
+    $SMIT issue clone1/$PROJECT1 1 -ph
+    $SMIT issue clone1/$PROJECT1 2 -ph
+    $SMIT issue clone1/$PROJECT1 3 -ph
+    $SMIT issue clone1/$PROJECT1 4 -ph
+) | grep -v "^Date:" > $TEST_NAME.out
 
-ISSUE=clone1/$PROJECT1/issues/3
-checkFile $ISSUE/entry3x1server
-checkFile $ISSUE/entry3x2server
-n=`ls $ISSUE | wc -l`
-[ $n -eq 2 ] || fail "[clone1/$PROJECT1/issues/3] n=$n"
-
-ISSUE=clone1/$PROJECT1/issues/4
-checkFile $ISSUE/entry3x1local
-checkFile $ISSUE/entry3x2local
-n=`ls $ISSUE | wc -l`
-[ $n -eq 2 ] || fail "[$ISSUE] n=$n"
-
-#checkFile $PROJECT1/issues/1/entry1x3
-
-
+diff -u $srcdir/$TEST_NAME.ref $TEST_NAME.out
 
