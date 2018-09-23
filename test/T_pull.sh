5927c38c14ab17e4feb9195b19ab77c020813549
Frederic Hoerni <fhoerni@free.fr>
2018-09-23 17:55:13 +0200

    test/T_pull: reduce duplicate code

--- test/T_pull.sh
+++ test/T_pull.sh
@@ -6,26 +6,21 @@ TEST_NAME=`basename $0`
 # remove suffix .sh
 TEST_NAME=`echo $TEST_NAME | sed -e "s/\.sh//"`
 exec > $TEST_NAME.log 2>&1
+rm -f $TEST_NAME.out
 
 SMIT=../smit
 
 set -e 
 
-REPO=trepo
-PROJECT1=p1
-USER1=tuser1
-PASSWD1=tpasswd1
-PORT=8099
+. $srcdir/functions
+
+initTest
 
 init() {
-    mkdir $REPO
-    $SMIT init $REPO
-    $SMIT project -c $REPO/$PROJECT1
+	initEmptyRepo
+
     # add custom property
     $SMIT project $REPO/$PROJECT1 addProperty "color select blue green yellow"
-    $SMIT project $REPO/$PROJECT1 addProperty "freeText text"
-
-    $SMIT user $USER1 --passwd $PASSWD1 --project $PROJECT1:rw -d $REPO
 
     # create some entries
     # create issue 1
@@ -41,43 +36,23 @@ cleanup() {
     rm -rf clone1
 }
 
-startServer() {
-    $SMIT serve $REPO --listen-port $PORT &
-    pid=$!
-    sleep 0.25 # wait for the server to start
-}
-stopServer() {
-    echo killing pid=$pid
-    kill $pid
-}
-
-fail() {
-    echo "ERROR: $1"
-    [ -n "$pid" ] && kill $pid
-    exit 1
-}
-checkFile() {
-    file="$1"
-    [ -f $file ] || fail "missing file '$file'"
-    echo "$file: ok"
-}
-
 cleanup
 init
 startServer
 
-# check that clone of p1 has correct content
+# smit-clone
 $SMIT clone http://127.0.0.1:$PORT --user $USER1 --passwd $PASSWD1 clone1
 
+# smit-pull
 cd clone1
 ../$SMIT pull
 cd -
 
 stopServer
 
-# add an entry server-side
+# add an entry (server-side)
 $SMIT issue $REPO/$PROJECT1 -a 1 color=yellow freeText="hello world"
-# add an issue
+# add an issue (server-side)
 $SMIT issue $REPO/$PROJECT1 -a - summary="t---d issue" freeText="this is xyz"
 
 # add attached files to issues #2 and #3
@@ -94,11 +69,15 @@ cd clone1
 cd -
 stopServer
 
-# check that the new entry and the new issue are pulled
-$SMIT issue clone1/$PROJECT1 1 --history --properties | grep -v "^Date:" > $TEST_NAME.out
-$SMIT issue clone1/$PROJECT1 2 --history --properties | grep -v "^Date:" >> $TEST_NAME.out
-$SMIT issue clone1/$PROJECT1 3 --history --properties | grep -v "^Date:" >> $TEST_NAME.out
+# check that the new entry and the new issue have been pulled
+t_runcmd $SMIT issue clone1/$PROJECT1 1 --history --properties
+t_runcmd $SMIT issue clone1/$PROJECT1 2 --history --properties
+t_runcmd $SMIT issue clone1/$PROJECT1 3 --history --properties
 
-diff -u $srcdir/$TEST_NAME.ref $TEST_NAME.out 
+# filter out dates
+sed -e "/^Date:/d" $TEST_NAME.out > $TEST_NAME.out.fil
+
+# check conformance
+diff -u $srcdir/$TEST_NAME.ref $TEST_NAME.out.fil
 
 
